fc85cb6eebb4652fe344a50efb8096bfd8efa26c
Fraser <fraser.chapman@gmail.com>
2024-01-28 13:49:50 +0000

    Update gone.sh
    update export

--- gone.sh
+++ gone.sh
@@ -2,14 +2,12 @@
 set -e
 
 if command -v go &> /dev/null; then
-    rm -rf usr/local/go
+    sudo rm -rf /usr/local/go/
     sed -i '/export GOPATH=$HOME\/go/d' ~/.profile
-    sed -i '/export PATH=usr\/local\/go\/bin:$PATH/d' ~/.profile
-    unset GOPATH
-    export PATH=$(echo $PATH | sed 's/:$GOPATH\/bin//')
-    source ~/.profile
+    export PATH=$(echo $PATH | sed 's|/usr/local/go/bin:||')
 
     echo "Go removed successfully."
+    echo "Update your shell environment by running 'source ~/.profile'"
 else
     echo "Go is not installed. Nothing to remove."
 fi
