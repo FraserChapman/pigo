9adcbc1f7668780641ba003ad094d90afd16845f
Fraser <fraser.chapman@gmail.com>
2024-01-28 13:48:28 +0000

    Update goget.sh
    export update

--- goget.sh
+++ goget.sh
@@ -8,28 +8,37 @@ if [ $# -eq 0 ]; then
 fi
 
 GO_VERSION=$1
-GO_URL="https://go.dev/dl/go$GO_VERSION.linux-arm64.tar.gz"
+GO_GZ="go$GO_VERSION.linux-arm64.tar.gz"
+GO_URL="https://go.dev/dl/$GO_GZ"
 
 if ! command -v go &> /dev/null; then
-    sudo apt update && sudo apt install -y wget git build-essential
+    sudo apt update && sudo apt install -y git build-essential
 
-    wget $GO_URL
-    tar -xvf go$GO_VERSION.linux-arm64.tar.gz
+    curl -4 -L -o $GO_GZ $GO_URL
+    tar -xvf $GO_GZ
     sudo mv go /usr/local
 
     echo 'export GOPATH=$HOME/go' >> ~/.profile
     echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.profile
-    source ~/.profile
+
+    # Apply changes to the current shell session
+    export GOPATH=$HOME/go
+    export PATH=/usr/local/go/bin:$PATH
 
     # Cleanup
-    rm go$GO_VERSION.linux-arm64.tar.gz
+    rm $GO_GZ
 
     # Check Go version
-    installed_version=$(go version | awk '{print $3}' | sed 's/go//')
-    if [ "$installed_version" = "$GO_VERSION" ]; then
-        echo "Go $GO_VERSION installed successfully."
+    if go version &> /dev/null; then
+        installed_version=$(go version | awk '{print $3}' | sed 's/go//')
+        if [ "$installed_version" = "$GO_VERSION" ]; then
+            echo "Go $GO_VERSION installed successfully."
+        else
+            echo "Failed to install Go $GO_VERSION. Installed version: $installed_version"
+            exit 1
+        fi
     else
-        echo "Failed to install Go $GO_VERSION. Installed version: $installed_version"
+        echo "Failed to verify Go installation. Please check manually."
         exit 1
     fi
 else
