158ac0c950f22c9a11dfd5efe218c837dfaf47c1
Fraser <fraser.chapman@gmail.com>
2024-01-28 11:13:25 +0000

    Update gone.sh
    Updated paths

--- gone.sh
+++ gone.sh
@@ -2,9 +2,9 @@
 set -e
 
 if command -v go &> /dev/null; then
-    rm -rf ~/.local/share/go
-    sed -i '/export GOPATH=$HOME\/.local\/share\/go/d' ~/.profile
-    sed -i '/export PATH=$HOME\/.local\/share\/go\/bin:$PATH/d' ~/.profile
+    rm -rf usr/local/go
+    sed -i '/export GOPATH=$HOME\/go/d' ~/.profile
+    sed -i '/export PATH=usr\/local\/go\/bin:$PATH/d' ~/.profile
     unset GOPATH
     export PATH=$(echo $PATH | sed 's/:$GOPATH\/bin//')
     source ~/.profile
